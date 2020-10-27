02828b477234b95880b3352c386393086a321b20
Rob Marwood <rob.marwood@adnsgroup.com>
2020-10-27 18:13:39 +0000

    Change hello world to hello git

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,7 +6,7 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worlds!");
+            Console.WriteLine("Hello Git!");
         }
     }
 }
